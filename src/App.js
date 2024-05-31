920a091c1b21d6c7221306a11ef975d8d34d244f
alexradomskiii <alexrad@umich.edu>
2024-05-30 20:13:25 -0400

    finished shop + cart pages

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/NavBar";
+import { Shop } from "./pages/shop/Shop";
+import { Cart } from "./pages/cart/Cart";
+import { ShopContextProvider } from "./context/Shop-context";
+
+
+function App() {
+  return (
+    <div className="App">
+      <ShopContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path='/' element={<Shop />} />
+            <Route exact path='/cart' element={<Cart />} />
+          </Routes>
+        </Router>
+      </ShopContextProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/products.js
@@ -0,0 +1,87 @@
+import iphone from "./assets/iphone.png";
+import galaxy from "./assets/samsung.jpeg";
+import beats from "./assets/beats.jpg";
+import alexa from "./assets/amazonalexa.webp";
+import hatch from "./assets/hatch.jpg";
+import speaker from "./assets/jblspeaker.jpg.webp";
+import macbook from "./assets/macbook.jpeg";
+import surface from "./assets/windows.jpg";
+import roku from "./assets/roku.png.webp";
+import ipad from "./assets/ipad.jpeg";
+import camera from "./assets/sony.jpg";
+import imac from "./assets/imac.jpeg";
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone",
+        price: 999.0,
+        productImage:iphone
+    },
+    {
+        id: 2,
+        productName: "Samsung Galaxy",
+        price: 899.0,
+        productImage:galaxy
+    },
+    {
+        id: 3,
+        productName: "Beats Headphones",
+        price: 399.0,
+        productImage:beats
+    },
+    {
+        id: 4,
+        productName: "Amazon Alexa",
+        price: 299.0,
+        productImage:alexa
+    },
+    {
+        id: 5,
+        productName: "Hatch Alarm Clock",
+        price: 199.0,
+        productImage:hatch
+    },
+    {
+        id: 6,
+        productName: "JBL Speaker",
+        price: 99.0,
+        productImage:speaker
+    },
+    {
+        id: 7,
+        productName: "Macbook",
+        price: 1899.0,
+        productImage:macbook
+    },
+    {
+        id: 8,
+        productName: "Windows Surface Laptop",
+        price: 1499.0,
+        productImage:surface
+    },
+    {
+        id: 9,
+        productName: "Roku TV",
+        price: 599.0,
+        productImage:roku
+    },
+    {
+        id: 10,
+        productName: "IPad",
+        price: 699.0,
+        productImage:ipad
+    },
+    {
+        id: 11,
+        productName: "Sony Camera",
+        price: 299.0,
+        productImage:camera
+    },
+    {
+        id: 12,
+        productName: "IMac",
+        price: 1999.0,
+        productImage:imac
+    },
+];
